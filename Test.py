9bcfaa47db9f8f85dcc997bef61c75a9b3c1bf6c
v-mipeng <pengminlong@outlook.com>
2016-05-24 09:27:11 +0800

    state commit

--- Test.py
+++ Test.py
@@ -1,2 +1,13 @@
-import script
+words = set()
+with open(r"D:\Codes\Project\EntityTyping\Neural Entity Typing\input\tables\satori and bbn\word2id.txt","r") as f:
+    for line in f:
+        words.add(line.strip().split('\t')[0])
+writer = open(r"D:\Codes\Project\EntityTyping\Neural Entity Typing\input\tables\word embedding.txt","w+")
+with open(r"D:\Data\Google-word2vec\GoogleNews-vectors-negative300.txt","r") as reader:
+    for line in reader:
+        word = line.strip().split(' ',1)[0]
+        if word in words:
+            writer.write(line)
+writer.close()
+    
 
