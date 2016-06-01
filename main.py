1edc55694b55c56aa12d6dae52f70d20824cfce1
v-mipeng <pengminlong@outlook.com>
2016-06-01 14:01:26 +0800

    state commit: Reconstruct the system with better structure and inheritance

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("./source")
+from entrance.multi_time_lstm import MTLE
+
+model = MTLE()
+
+# Train model with default training dataset
+model.train(train_path = None, valid_portion = None, model_path = None)
+
+# Test on default dataset
+model.test(test_path = None, test_result_path = None, model_path = None)
+
+# Predict on default dataset
+model.predict(predict_path = None, predict_result_path = None, model_path = None)
